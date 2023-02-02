e08b79b4029357d9efd9ef781311ae66fd9bfed2
bonveeee <bonochieng@gmail.com>
2023-02-02 16:34:14 +0300

    deployment

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+
 const command = process.argv[2] || "build";
 const args = [
   "tailwindcss",
-  command === "build" ? "build" : "watch",
-  "--output",
-  "public",
+  "-i",
+  "./input.css",
+  "-o",
+  "./css/main.css",
   ...(command === "watch" ? ["--watch"] : [])
 ];
 
 console.log(`Running command: ${command}`);
 console.log(`With arguments: ${args.join(" ")}`);
+
